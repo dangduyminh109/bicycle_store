a48432610ce4e774673f5d4d65bce5b70ce7e822
dangduyminh109 <dangduyminh109@gmail.com>
2025-05-17 18:20:23 +0700

    hoàn thành trang chi tiết sản phẩm

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -1,25 +1,27 @@
 function handleProduct() {
     const countdown = document.getElementById("countdown");
-    var minute = 59;
-    var second = 60;
-    var hour = 11;
-    const box = countdown.querySelectorAll("strong");
-    setInterval(() => {
-        box[2].innerText = String(second--).padStart(2, "0");
-        box[1].innerText = String(minute).padStart(2, "0");
-        box[0].innerText = String(hour).padStart(2, "0");
-        if (hour == 0 && minute == 0 && second == 0) {
-            hour = 12;
-        }
-        if (minute == 0 && second == 0) {
-            minute = 60;
-            --hour;
-        }
-        if (second == 0) {
-            --minute;
-            second = 60;
-        }
-    }, 1000);
+    if (countdown) {
+        var minute = 59;
+        var second = 60;
+        var hour = 11;
+        const box = countdown.querySelectorAll("strong");
+        setInterval(() => {
+            box[2].innerText = String(second--).padStart(2, "0");
+            box[1].innerText = String(minute).padStart(2, "0");
+            box[0].innerText = String(hour).padStart(2, "0");
+            if (hour == 0 && minute == 0 && second == 0) {
+                hour = 12;
+            }
+            if (minute == 0 && second == 0) {
+                minute = 60;
+                --hour;
+            }
+            if (second == 0) {
+                --minute;
+                second = 60;
+            }
+        }, 1000);
+    }
 }
 
 function handleCollection() {
