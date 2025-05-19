9959ea750372a11c51afc6b576baf3654799998e
dangduyminh109 <dangduyminh109@gmail.com>
2025-05-19 13:46:56 +0700

    hoàn thành form đăng kí, đăng nhập

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -41,8 +41,124 @@ function handleCollection() {
     }
 }
 
+async function handleAuth() {
+    const passwordIcon = document.querySelectorAll(".password__icon");
+    if (passwordIcon) {
+        passwordIcon.forEach((element) => {
+            element.onclick = () => {
+                const passwordInput = element
+                    .closest(".password__group")
+                    .querySelector("input");
+                if (passwordInput.type === "password") {
+                    passwordInput.type = "text";
+                    element.firstChild.classList.remove("fa-eye-slash");
+                    element.firstChild.classList.add("fa-eye");
+                } else {
+                    passwordInput.type = "password";
+                    element.firstChild.classList.remove("fa-eye");
+                    element.firstChild.classList.add("fa-eye-slash");
+                }
+            };
+        });
+    }
+
+    const registerLinkBtn = document.getElementById("register-link__btn");
+    const loginLinkBtnRegister = document.getElementById(
+        "login-link__btn--register"
+    );
+    const loginLinkBtnForgot = document.getElementById(
+        "login-link__btn--forgot"
+    );
+    const forgotLinkBtn = document.getElementById("forgot-link__btn");
+    if (
+        registerLinkBtn &&
+        loginLinkBtnRegister &&
+        forgotLinkBtn &&
+        loginLinkBtnForgot
+    ) {
+        const auth = document.getElementById("auth");
+        loginLinkBtnRegister.onclick = () => {
+            auth.style.transform = "translateX(calc(-200% / 3))";
+        };
+        registerLinkBtn.onclick = () => {
+            auth.style.transform = "translateX(calc(-100% / 3))";
+        };
+        forgotLinkBtn.onclick = () => {
+            auth.style.transform = "translateX(calc(-100% / 3))";
+        };
+        loginLinkBtnForgot.onclick = () => {
+            auth.style.transform = "translateX(0)";
+        };
+    }
+
+    const authFrom = document.querySelectorAll(".authForm");
+    if (authFrom) {
+        authFrom.forEach((item) => {
+            const inputData = item.querySelectorAll("input");
+            inputData.forEach((input) => {
+                input.onkeyup = () => {
+                    if (input.value.trim() != "") {
+                        input.classList.remove("error");
+                        if (
+                            input.name !== "password" &&
+                            input.name !== "passwordConfirm"
+                        ) {
+                            input.nextElementSibling.style.visibility =
+                                "hidden";
+                        } else {
+                            input.parentNode.nextElementSibling.style.visibility =
+                                "hidden";
+                        }
+                    }
+                };
+            });
+        });
+    }
+
+    const btnSubmit = document.querySelectorAll(".btn-submit-auth");
+    if (btnSubmit) {
+        btnSubmit.forEach((item) => {
+            item.onclick = async (e) => {
+                const form = item.closest("form");
+                const inputData = form.querySelectorAll("input");
+                inputData.forEach((input) => {
+                    if (input.value.trim() === "") {
+                        e.preventDefault();
+                        input.classList.add("error");
+                        if (
+                            input.name !== "password" &&
+                            input.name !== "passwordConfirm"
+                        ) {
+                            input.nextElementSibling.style.visibility =
+                                "visible";
+                        } else {
+                            input.parentNode.nextElementSibling.style.visibility =
+                                "visible";
+                        }
+                    }
+                });
+            };
+        });
+    }
+
+    const btnConfirm = document.querySelectorAll(".btn-confirm");
+    if (btnConfirm) {
+        btnConfirm.forEach((btn) => {
+            btn.onclick = () => {
+                const form = btn.closest("form");
+                const inputEmail = form.querySelector("input[type=email]");
+                if (inputEmail && inputEmail.value.trim() === "") {
+                    inputEmail.classList.add("error");
+                    inputEmail.nextElementSibling.style.visibility = "visible";
+                }
+            };
+        });
+    }
+}
+
 function init() {
     handleProduct();
     handleCollection();
+    handleAuth();
 }
 init();
